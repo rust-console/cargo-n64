5b30b3558107ffb491e18441430b924a8b076fd2
Kjartan Skjervøy <kskjer@gmail.com>
2020-02-19 00:28:25 +0100

    Add Newlib binary and allocator support
    Adds builds of newlib-3.3.0, and implement an allocator in n64lib which
    uses this.
    
    Newlib was configured as follows:
    
        CFLAGS_FOR_TARGET="-DPREFER_SIZE_OVER_SPEED=1 -Os -G0 -mabi=32 -mno-gpopt" \
            ../configure --target=mips-elf
    
    libc.a and libm.a are bundled in the cargo-n64 executable, and written
    to the temp directory where the other temporary build artifacts are
    placed (linker script, for example).
    
    Since our sbrk() starts returning memory at the end of the .bss section,
    up the alignment of this in the linker script to 16 bytes.
    
    Also enable the 'noabicalls' feature in the target.json. This is the
    default in GCC with the mips-elf target, but not for LLVM. The linker
    will emit some warnings without this.

---
+++ examples/n64lib/src/allocator.rs
@@ -0,0 +1,53 @@
+use crate::lock::interrupt_lock;
+use core::alloc::{GlobalAlloc, Layout};
+
+#[link(name = "c")]
+extern "C" {
+    fn malloc(size: usize) -> *mut u8;
+    fn free(ptr: *mut u8);
+}
+
+struct N64LibAlloc;
+
+unsafe impl GlobalAlloc for N64LibAlloc {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let _lock = interrupt_lock();
+
+        malloc(layout.size())
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+        let _lock = interrupt_lock();
+
+        free(ptr);
+    }
+}
+
+#[global_allocator]
+static GLOBAL: N64LibAlloc = N64LibAlloc;
+
+#[alloc_error_handler]
+fn alloc_error(_layout: Layout) -> ! {
+    loop {}
+}
+
+extern "C" {
+    static __bss_end: u8;
+}
+
+// Newlib's malloc implementation calls sbrk when it needs more memory.
+//
+// sbrk() maintains a static pointer to the end of the program's image in memory (__bss_end), and
+// increments it as needed.
+//
+// Shouldn't need any locking here since this function is only called from malloc().
+#[no_mangle]
+extern "C" fn sbrk(increment: usize) -> *const u8 {
+    unsafe {
+        static mut PTR: &'static u8 = unsafe { &__bss_end };
+
+        PTR = (PTR as *const u8).add(increment).as_ref().unwrap();
+
+        PTR as *const u8
+    }
+}
--- examples/n64lib/src/lib.rs
+++ examples/n64lib/src/lib.rs
@@ -1,5 +1,9 @@
 #![warn(rust_2018_idioms)]
 #![no_std]
+#![feature(alloc_error_handler)]
+#![feature(asm)]
 
+mod allocator;
 pub mod ipl3font;
+mod lock;
 pub mod vi;
