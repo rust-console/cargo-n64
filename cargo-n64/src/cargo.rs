5bfe9e17b44685a0a6a0e0e7d6bf1c9c84c54e6c
Jay Oster <jay@kodewerx.org>
2022-06-16 06:09:06 -0700

    Duplicate the rust-toolchain file so the crate can be published (#51)
    - This is unfortunate, but the crate must include a copy of the rust-toolchain file since it needs to build ROMs with this version.
    - When updating the toolchain file, both copies need to be kept in sync.
    - Maybe it is worth writing a build script that will ensure they are identical...
      - Sounds challenging! The build script must work when building from the repo and when building in isolation (downloaded from crates.io) It would have to be at least smart enough to figure out which scenario it is building in.
      - I'll defer that until updating the duplicate toolchain file becomes a problem.

--- cargo-n64/src/cargo.rs
+++ cargo-n64/src/cargo.rs
@@ -86,7 +86,7 @@ pub(crate) fn run(args: &cli::BuildArgs, verbose: usize) -> Result<CargoArtifact
     };
 
     let output = Command::new("cargo")
-        .arg(format!("+{}", include_str!("../../rust-toolchain").trim()))
+        .arg(format!("+{}", include_str!("../rust-toolchain").trim()))
         .arg("build")
         .arg("-Z=build-std=core,alloc")
         .arg("--message-format=json-render-diagnostics")
